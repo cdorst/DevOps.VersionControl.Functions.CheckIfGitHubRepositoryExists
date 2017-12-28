ea5b62fe38c6799dadf3115f1d24214ef6867cc4
Christopher Dorst <chris@dorst.email>
2017-12-27 19:13:53 -0600

    Adding GitHubRepositoryExistenceChecker

---
+++ DevOps.VersionControl.Functions.CheckIfGitHubRepositoryExists/GitHubRepositoryExistenceChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using static Common.Functions.GetHttpStatusCode.HttpStatusCodeGetter;
+
+namespace DevOps.VersionControl.Functions.CheckIfGitHubRepositoryExists
+{
+    public static class GitHubRepositoryExistenceChecker
+    {
+        private const string GitHubUri = "https://github.com";
+
+        public static async Task<bool> Exists(string account, string repository)
+            => (await Status(account, repository)) == HttpStatusCode.OK;
+
+        private static async Task<HttpStatusCode> Status(string account, string repository)
+            => await GetStatusCode(Uri(account, repository));
+
+        private static string Uri(string account, string repository)
+            => $"{GitHubUri}/{account}/{repository}";
+    }
+}
