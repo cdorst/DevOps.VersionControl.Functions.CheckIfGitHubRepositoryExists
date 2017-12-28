948d3a1321df0cfe9cf4d54c8e2da78d6a7fb141
Christopher Dorst <chris@dorst.email>
2017-12-28 00:41:23 -0600

    Using AccountRepository structure

--- DevOps.VersionControl.Functions.CheckIfGitHubRepositoryExists/GitHubRepositoryExistenceChecker.cs
+++ DevOps.VersionControl.Functions.CheckIfGitHubRepositoryExists/GitHubRepositoryExistenceChecker.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using DevOps.VersionControl.Structures.GitHub;
+using System.Net;
 using System.Threading.Tasks;
 using static Common.Functions.GetHttpStatusCode.HttpStatusCodeGetter;
 
@@ -6,15 +7,10 @@ namespace DevOps.VersionControl.Functions.CheckIfGitHubRepositoryExists
 {
     public static class GitHubRepositoryExistenceChecker
     {
-        private const string GitHubUri = "https://github.com";
+        public static async Task<bool> Exists(AccountRepository accountRepository)
+            => (await Status(accountRepository)) == HttpStatusCode.OK;
 
-        public static async Task<bool> Exists(string account, string repository)
-            => (await Status(account, repository)) == HttpStatusCode.OK;
-
-        private static async Task<HttpStatusCode> Status(string account, string repository)
-            => await GetStatusCode(Uri(account, repository));
-
-        private static string Uri(string account, string repository)
-            => $"{GitHubUri}/{account}/{repository}";
+        private static async Task<HttpStatusCode> Status(AccountRepository accountRepository)
+            => await GetStatusCode(accountRepository.Uri);
     }
 }
